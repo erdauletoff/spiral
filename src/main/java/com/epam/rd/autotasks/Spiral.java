ad53802aa7e54d409ee2fd8292ecdf4fb27974f0
Darkhan Erdawletov <darkhan_erdawletov@student.itpu.uz>
2024-10-27 22:16:57 +0500

    solution 2

--- src/main/java/com/epam/rd/autotasks/Spiral.java
+++ src/main/java/com/epam/rd/autotasks/Spiral.java
@@ -35,4 +35,40 @@ class Spiral {
 
         return matrix;
     }
+
+//    solution 2
+//    static int[][] spiral(int rows, int columns) {
+//        int[][] matrix = new int[rows][columns];
+//        int num = 1;
+//        int i = 0, j = 0;
+//        int d_row = 0, d_col = 1;
+//
+//        while (num <= rows * columns) {
+//            matrix[i][j] = num++;
+//            int next_i = i + d_row;
+//            int next_j = j + d_col;
+//
+//            if (next_i < 0 || next_i >= rows || next_j < 0 || next_j >= columns || matrix[next_i][next_j] != 0) {
+//                // Change direction
+//                if (d_col == 1) { // right to down
+//                    d_col = 0;
+//                    d_row = 1;
+//                } else if (d_row == 1) { // down to left
+//                    d_col = -1;
+//                    d_row = 0;
+//                } else if (d_col == -1) { // left to up
+//                    d_col = 0;
+//                    d_row = -1;
+//                } else if (d_row == -1) { // up to right
+//                    d_col = 1;
+//                    d_row = 0;
+//                }
+//            }
+//
+//            i += d_row;
+//            j += d_col;
+//        }
+//
+//        return matrix;
+//    }
 }
